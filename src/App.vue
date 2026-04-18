3ddf388b507beed0176ad0524c41950e761a847d
TheMrQ <tuanquynhlol@gmail.com>
2026-04-18 19:17:39 +0700

    Rollback

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <transition name="page-fade" mode="out-in">
-      <component :is="Component" />
-    </transition>
-  </router-view>
+  <div class="min-h-screen bg-slate-50 font-sans text-slate-900">
+    <RouterView />
+  </div>
 </template>
 
-<style>
-/* Global Smooth Page Transition */
-.page-fade-enter-active,
-.page-fade-leave-active {
-  transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
-}
-.page-fade-enter-from {
-  opacity: 0;
-  transform: scale(0.98) translateY(10px);
-}
-.page-fade-leave-to {
-  opacity: 0;
-  transform: scale(0.98) translateY(-10px);
-}
-
-/* Ensure global fonts are smooth */
-body {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
